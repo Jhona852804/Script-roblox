f0cdbbb53136eccc885e82cf8587592bd39614e5
Jhona852804 <120608656+Jhona852804@users.noreply.github.com>
2025-06-27 11:27:25 -0300

    Update ByJhonaDev139-new.lua
    Signed-off-by: Jhona852804 <120608656+Jhona852804@users.noreply.github.com>

--- ByJhonaDev139-new.lua
+++ ByJhonaDev139-new.lua
@@ -57,7 +57,7 @@ screenGui.Parent = game.CoreGui
 -- Cria a janela principal (MainFrame)
 local mainFrame = Instance.new("Frame")
 mainFrame.Name = "MainFrame"
-mainFrame.Size = UDim2.new(0, 480, 0, 330)
+mainFrame.Size = UDim2.new(0, 488, 0, 338)
 mainFrame.Position = UDim2.new(0.5, -200, 0.5, -150)
 mainFrame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
 mainFrame.BackgroundTransparency = 0
